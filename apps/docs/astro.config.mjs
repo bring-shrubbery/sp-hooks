56c65730a2efedb6861601e85dc67ed6140af103
Antoni Silvestrovic <antoni.silvestrovic@gmail.com>
2023-11-05 21:00:12 +0200

    Updated site url

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://use-search-params-state.vercel.app/",
   integrations: [
     starlight({
       title: "use-search-params-state docs",
